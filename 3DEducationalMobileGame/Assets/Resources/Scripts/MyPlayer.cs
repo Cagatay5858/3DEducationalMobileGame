b38ec9904998aa4b4886f0ac6019664d93c4c7be
Yasin Çağatay Özbek <yasincagatayozbek@gmail.com>
2025-11-25 16:31:27 +0300

    no animated movement system added

--- 3DEducationalMobileGame/Assets/Resources/Scripts/MyPlayer.cs
+++ 3DEducationalMobileGame/Assets/Resources/Scripts/MyPlayer.cs
@@ -2,15 +2,16 @@ using UnityEngine;
 
 public class MyPlayer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 input = new(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        Vector2 inputDir = input.normalized;
+
+        if(inputDir != Vector2.zero)
+        transform.eulerAngles = Vector3.up * Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg;
+
+        transform.Translate(transform.forward * (5f * inputDir.magnitude) * Time.deltaTime, Space.World);
     }
 }
